407dc5e3aa10a94b6ca575a2a5ed00187d448533
PriyankaBtech <officialpriyanka2000@gmail.com>
2024-02-07 18:59:09 +0530

    basic theory of routing

---
+++ BasicTheory/theory.js
@@ -0,0 +1,14 @@
+// Routing generally works in RESTful APIs:
+
+/**
+HTTP Methods:
+- HTTP methods such as GET, POST, PUT, DELETE, etc., are used to perform different actions on resources. For example:
+
+GET /users: Retrieve a list of users.
+GET /users/123: Retrieve the user with ID 123.
+POST /users: Create a new user.
+PUT /users/123: Update the user with ID 123.
+DELETE /users/123: Delete the user with ID 123.
+ 
+
+*/
